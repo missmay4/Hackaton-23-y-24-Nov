e02c9d7ac52e39e6f09fccc5660b90d303328ffa
Unknown <prinpio@gmail.com>
2018-01-09 12:02:07 +0100

    Añadida libreria jQuery a js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
+
+//----------GESTION
+
 // Validacion JS gestion galeria
-$.validate({
-  lang: 'es'
+
+
+// $.validate({
+//   lang: 'es'
+// });
+jQuery.validator.setDefaults({
+  debug: true,
+  success: "valid"
 });
+$( "#mailReg" ).validate({
+  rules: {
+    field: {
+      required: true,
+      email: true
+    }
+  }
+});
+
+//--------------FIN GESTION
