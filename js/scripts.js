15b0e28580765e2bf2234142b2bce34810343f10
Mayte Mac <may.eater@gmail.com>
2018-01-09 16:16:36 +0100

    Multiples cambios en index + popup
    Index: Bootstrap, redimension headerimagen
    Popup: Cimientos html y js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 // Validacion JS gestion galeria
-$.validate({
-  lang: 'es'
-});
+
+// Ventana emergente comentarios
+
+/* $(document).ready(function() {
+  $(function() {
+    $("#dialog").dialog({
+      autoOpen: false
+    });
+    $("#emergente").on("click", function() {
+      $("#dialog").dialog("open");
+    });
+  });
+  // Validating Form Fields.....
+  $("#submit").click(function(e) {
+    var comentario = $("#comentario").val();
+
+  });
+}); */
+
+// Fin ventana emergente
