295a39392fd54214bb5004fca398a603e0e4f387
Unknown <prinpio@gmail.com>
2018-01-11 12:35:09 +0100

    hack Js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,4 @@
 
-/*// Ventana emergente comentarios 
-
-// Coge la ventana modal
-var modal = document.getElementById('myModal');
-// Coge el boton que esta en la vista detalle y abre la ventana modal
-var buttonOpen = document.getElementById("buttonComent");
-// Coge el boton que envia el comentario
-var buttonClose = document.getElementById("myButton");
-// When the user clicks on the button, open the modal
-function abrirVentana() {
-  //console.log("Hola");
-  modal.style.display = "block";
-}
-// Cuando pulse en aceptar, cerrar la ventana modal
-function cerrarModal() {
-  //console.log("Adios");
-  modal.style.display = "none";
-}
-function limpiarText() {
-  document.getElementById("comentario").value = "";
-}
-
-// Fin ventana emergente*/
-
-//----- VISTA DETALLE
-// -----JS Vista Detalle VALORACIÃ“N Estrellas
-
-/*
-$(function () {
- 
-  $("#rateYo").rateYo({
-    rating: 3.6
-  });
-
-
-})
-*/
-//----- fin VISTA DETALLE
 
 
 $( document ).ready(
@@ -160,8 +122,6 @@ function loadData(){
 
 
 
-
-
 /*//FUNCION JS PARA JAVASCRIPT
 
 
@@ -184,21 +144,4 @@ return false;
 
 //--------------FIN GESTION
 
-<<<<<<< Updated upstream
-=======
-
-
-//----- VISTA DETALLE
-// -----JS Vista Detalle VALORACIÓN Estrellas
-
->>>>>>> Stashed changes
-
-
-
-
-
-<<<<<<< Updated upstream
-=======
-  
->>>>>>> Stashed changes
 
